464573b793cfd04793e4be0e9bc5e4b6036b76a0
Pau Casas Garcia <pacasasgar@gmail.com>
2019-10-16 21:01:18 +0200

    Added pointers info

--- 02_Pointers/02_Pointers/Main.cpp
+++ 02_Pointers/02_Pointers/Main.cpp
@@ -15,6 +15,19 @@ int main() {
 
 	cout << "*&num1 is: " << *&num1 << endl;
 
+	num1 = *p1 + 2;
+	cout << "num1 after suma is: " << num1 << endl;
+	cout << "p1 is: " << p1 << endl;
+	cout << "*p1 is: " << *p1 << endl;
+
+	//arrays
+	string texts[] = {"one", "two", "three"};
+	cout << "sizeof(texts) is: " << sizeof(texts) << endl;
+	cout << "sizeof(string) is: " << sizeof(string) << endl;
+	cout << "sizeof(texts)/sizeof(string) is: " << sizeof(texts)/ sizeof(string) << endl;		//not sure why, but this division gives us the LENGTH of the array
+	cout << "texts->length() is: " << texts->length();											//this also works for LENGTH
+
+
 	system("pause");
 	return 0;
 }

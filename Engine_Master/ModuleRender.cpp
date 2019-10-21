9b954d68755b8426c1a808c710e867af7d6ba359
Pau Casas Garcia <pacasasgar@gmail.com>
2019-10-21 21:15:48 +0200

    Added Engine_Master project

---
+++ Engine_Master/ModuleRender.cpp
@@ -0,0 +1,64 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleRender.h"
+#include "ModuleWindow.h"
+//#include "SDL.h"
+
+ModuleRender::ModuleRender()
+{
+}
+
+// Destructor
+ModuleRender::~ModuleRender()
+{
+	SDL_GL_DeleteContext(glcontext);
+}
+
+// Called before render is available
+bool ModuleRender::Init()
+{
+	LOG("Creating Renderer context");
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+
+    glcontext = SDL_GL_CreateContext(App->window->window);
+
+
+	return true;
+}
+
+update_status ModuleRender::PreUpdate()
+{
+	return UPDATE_CONTINUE;
+}
+
+// Called every draw update
+update_status ModuleRender::Update()
+{
+	return UPDATE_CONTINUE;
+}
+
+update_status ModuleRender::PostUpdate()
+{
+	return UPDATE_CONTINUE;
+}
+
+// Called before quitting
+bool ModuleRender::CleanUp()
+{
+	LOG("Destroying renderer");
+
+	//Destroy window
+
+	return true;
+}
+
+void ModuleRender::WindowResized(unsigned width, unsigned height)
+{
+}
+

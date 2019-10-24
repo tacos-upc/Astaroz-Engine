0cbfd839b4fc8f2a899bc745edd78c602e69a214
Pau Casas Garcia <pacasasgar@gmail.com>
2019-10-24 18:40:41 +0200

    Added libraries as relative paths + Triangle draw complete

--- Engine_Master/ModuleRender.cpp
+++ Engine_Master/ModuleRender.cpp
@@ -2,6 +2,7 @@
 #include "Application.h"
 #include "ModuleRender.h"
 #include "ModuleWindow.h"
+#include "glew.h";
 //#include "SDL.h"
 
 ModuleRender::ModuleRender()
@@ -28,23 +29,70 @@ bool ModuleRender::Init()
 
     glcontext = SDL_GL_CreateContext(App->window->window);
 
+	GLenum err = glewInit();
+	// … check for errors
+	LOG("Using Glew %s", glewGetString(GLEW_VERSION));
+	LOG("Vendor: %s", glGetString(GL_VENDOR));
+	LOG("Renderer: %s", glGetString(GL_RENDERER));
+	LOG("OpenGL version supported %s", glGetString(GL_VERSION));
+	LOG("GLSL: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	glClearDepth(1.0f);
+	glClearColor(0.f, 0.f, 0.f, 1.f);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_DEPTH_TEST);
+	glFrontFace(GL_CCW);
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_TEXTURE_2D);
+	glViewport(0, 0, 1024, 768);
 
 	return true;
 }
 
 update_status ModuleRender::PreUpdate()
 {
+	glViewport(0,0, SCREEN_WIDTH, SCREEN_HEIGHT);
+	//SDL_GetWindowSize();
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 	return UPDATE_CONTINUE;
 }
 
 // Called every draw update
 update_status ModuleRender::Update()
 {
+	float buffer_data[] = { -1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f };
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(buffer_data), buffer_data, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glEnableVertexAttribArray(0); // attribute 0
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(
+		0, // attribute 0
+		3, // number of componentes (3 floats)
+		GL_FLOAT, // data type
+		GL_FALSE, // should be normalized?
+		0, // stride
+		(void*)0 // array buffer offset
+	);
+	glDrawArrays(GL_TRIANGLES, 0, 3); // start at 0 and 3 tris
+	glDisableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
 	return UPDATE_CONTINUE;
 }
 
 update_status ModuleRender::PostUpdate()
 {
+	SDL_GL_SwapWindow(App->window->window);
 	return UPDATE_CONTINUE;
 }
 

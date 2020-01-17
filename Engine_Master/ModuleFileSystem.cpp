6eba018bea685da09c2a0921ea76f72d81674106
luismoyano <39064112+luismoyano@users.noreply.github.com>
2020-01-17 20:51:57 +0100

    First approach to physfs

--- Engine_Master/ModuleFileSystem.cpp
+++ Engine_Master/ModuleFileSystem.cpp
@@ -1,6 +1,7 @@
 #include "ModuleFileSystem.h"
-
-
+#include "physfs-3.0.2/src/physfs.h"
+#include "SDL.h"
+#include "Globals.h"
 
 ModuleFileSystem::ModuleFileSystem()
 {
@@ -11,12 +12,32 @@ ModuleFileSystem::~ModuleFileSystem()
 {
 }
 
-
-ModuleFileSystem::ModuleFileSystem()
+bool ModuleFileSystem::Init()
 {
+	PHYSFS_init(NULL);	
+	addToSearchPath(SDL_GetPrefPath("Astaroz", "AstarozEngine"));
+	
+	return true;
 }
 
-S
-ModuleFileSystem::~ModuleFileSystem()
+bool ModuleFileSystem::Start()
 {
+	return true;
+}
+
+bool ModuleFileSystem::CleanUp()
+{
+	PHYSFS_deinit();
+	return true;
+}
+
+bool ModuleFileSystem::Exist(const char* filePathAndName)
+{
+	return PHYSFS_exists(filePathAndName);
+}
+
+void ModuleFileSystem::addToSearchPath(const char* path)
+{
+	PHYSFS_addToSearchPath(path, searchPathPriority);
+	searchPathPriority += 1;
 }
--- Engine_Master/ModuleFileSystem.h
+++ Engine_Master/ModuleFileSystem.h
@@ -5,13 +5,27 @@ class ModuleFileSystem : public Module
 public:
 	ModuleFileSystem();
 	~ModuleFileSystem();
-};
 
-class ModuleFileSystem :
-	public Module
-{
-public:
-	ModuleFileSystem();
-	~ModuleFileSystem();
+	bool Init() override;
+	bool Start() override;
+	void CleanUp() override;
+
+	bool Exist(const char*);
+	unsigned int Size(const char*); 
+	bool CreateDir();
+	bool IsDir(const char*);
+	bool Delete();
+
+	bool openFileBrowser();
+	const char* selectFileInBrowser();
+
+	void save(const char*, const char*);
+	void load( const char*);
+
+private:
+	const char* fileSystemPath;
+	int searchPathPriority = 1;
+
+	void addToSearchPath(const char*);
 };
 

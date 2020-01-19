6664da1156a9ba70a6b26132025508d083487cfa
Luis Moyano <luis@luismoyano.net>
2020-01-19 17:29:46 +0100

    Mouse picking fixed

--- Engine_Master/ModuleEditorCamera.cpp
+++ Engine_Master/ModuleEditorCamera.cpp
@@ -305,6 +305,7 @@ float2 ModuleEditorCamera::cartesianToPolar(float2 cartesian, float2 target)
 
 void ModuleEditorCamera::raycast()
 {
+	LOG("HOVERED WINDOW: %s", App->editor->getHoveredWindowData()->name);
 	if (App->editor->isFocusedAndHovered("Scene") && App->input->GetMouseButtonDown(SDL_BUTTON_LEFT) && !ImGuizmo::IsOver())
 	{
 		float3 mousePosition = getMouseToViewportPosition();
